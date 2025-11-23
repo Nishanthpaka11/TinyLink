d0b6f19a36e9a286248dc9475427497ceed68a0a
Nishanthpaka11 <yisagi868@gmail.com>
2025-11-23 15:27:15 +0530

    Update server.js

--- tinylink-backend/server.js
+++ tinylink-backend/server.js
@@ -5,7 +5,9 @@ const port = process.env.PORT || 4000;
 app.get("/ping",(req,res)=>{
   res.status(200).send("pong");
 });
-
+app.get("/healthz", (req, res) => {
+  res.status(200).send("OK");
+});
 app.listen(port, () => {
   console.log(`TinyLink backend running on port ${port}`);
 });
