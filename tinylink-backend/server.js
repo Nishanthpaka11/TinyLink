c67be662c8a586124a4e5d3b20af6f96a3a3ec62
Nishanthpaka11 <yisagi868@gmail.com>
2025-11-23 11:41:22 +0530

    Update server.js

--- tinylink-backend/server.js
+++ tinylink-backend/server.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 const app = require("./app");
 
 const port = process.env.PORT || 4000;
+app.get("/ping",(req,res)=>{
+  res.status(200).send("pong");
+});
 
 app.listen(port, () => {
   console.log(`TinyLink backend running on port ${port}`);
